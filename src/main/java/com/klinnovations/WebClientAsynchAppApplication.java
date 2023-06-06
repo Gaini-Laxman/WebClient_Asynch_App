6c5f181fd4542bf71696ca2d742990db337228f3
Gaini Laxman <gainilaxman12@gmail.com>
2023-06-06 19:59:26 +0530

    first commit

---
+++ src/main/java/com/klinnovations/WebClientAsynchAppApplication.java
@@ -0,0 +1,13 @@
+package com.klinnovations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebClientAsynchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebClientAsynchAppApplication.class, args);
+	}
+
+}
